42f9ff60faf4e7c1d16be40daa7f69a237096379
Thomas Langlois <thomas.langlois@mac.com>
2023-01-09 15:30:37 +0000

    ui

--- web/web-app/src/components/Header.js
+++ web/web-app/src/components/Header.js
@@ -40,7 +40,6 @@ const Header = () => {
               <div class="row">
                 <div class="col centered pt-3">
                   <a href="https://docs.cardanobi.io" target="_blank" rel="noopener noreferrer" class="popup-button glow-on-hover">🔥Launch Docs
-                  &nbsp;<svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
                   </a>
                 </div>
                 <div class="col centered pt-3">
